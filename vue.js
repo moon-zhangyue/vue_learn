55a0013d885ae9297388f9326d40316af5bbdbb1
zhangyue <575865770@qq.com>
2022-07-04 14:44:47 +0800

    refactor: 组件参数校验与非Props特性

--- vue.js
+++ vue.js
@@ -9097,8 +9097,6 @@
           devtools.emit('init', Vue);
         } else {
           console[console.info ? 'info' : 'log'](
-            'Download the Vue Devtools extension for a better development experience:\n' +
-            'https://github.com/vuejs/vue-devtools'
           );
         }
       }
@@ -9106,9 +9104,6 @@
         typeof console !== 'undefined'
       ) {
         console[console.info ? 'info' : 'log'](
-          "You are running Vue in development mode.\n" +
-          "Make sure to turn on production mode when deploying for production.\n" +
-          "See more tips at https://vuejs.org/guide/deployment.html"
         );
       }
     }, 0);
