963cd1e1b24f11c80021e6a81f469e8c6d50f5e6
zhangyue <575865770@qq.com>
2022-07-05 16:46:53 +0800

    refactor: cli启动app

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,40 +1,12 @@
 <template>
-  <div>
-    <div>
-      <input v-model="inputValue">
-      <button @click="handleSubmit">提交</button>
-    </div>
-    <ul>
-      <todo-item v-for="(item,index) of list" :key="index" :content="item" :index="index" @delete="handleDelete">
-
-      </todo-item>
-    </ul>
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
 <script>
-import TodoItem from "./components/TodoItem"
-
 export default {
-  components: {
-    'todo-item': TodoItem
-  },
-  data() {
-    return {
-      inputValue: '',
-      list: []
-    }
-  },
-  methods: {
-    handleSubmit() {
-      this.list.push(this.inputValue)
-      this.inputValue = ''
-    },
-    handleDelete(index) {
-      this.list.splice(index, 1)
-    }
-  }
-  // name: 'App'
+  name: 'App'
 }
 </script>
 
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import TodoItem from '@/components/TodoItem'
+// import TodoItem from '@/components/TodoItem'
+import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
 
 Vue.use(Router)
 
@@ -8,8 +10,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'TodoItem',
-      component: TodoItem
+      name: 'Home',
+      component: Home
+    }, {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
