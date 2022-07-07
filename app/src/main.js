9f01722f3e4672d261f06caa3074c86f3c06fc63
zhangyue <575865770@qq.com>
2022-07-07 14:34:36 +0800

    refactor: 轮播图

--- app/src/main.js
+++ app/src/main.js
@@ -1,15 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require('swiper/dist/css/swiper.css')
+
 import Vue from 'vue'
 import App from './App'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import router from './router'
 import fastClick from 'fastclick'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+// import 'swiper/css'
+// import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ app/src/pages/home/components/Swiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide>
+      <img class='swiper-img'
+           src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_256x160_f977b5d4.jpg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img class='swiper-img' src="//img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_256x160_618d3a8e.jpg"
+           alt="">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOptions: {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.swiper-img
+  width: 100%
+</style>
