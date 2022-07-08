f9e54b32bfc52addc335d0394d72d81a26533bb5
zhangyue <575865770@qq.com>
2022-07-08 16:42:44 +0800

    refactor:自定义信息

--- app/src/pages/home/components/Icons.vue
+++ app/src/pages/home/components/Icons.vue
@@ -80,6 +80,9 @@ export default {
   height: 0
   padding-bottom 50%
 
+icons
+  margin-top .1rem
+
 .icon
   position: relative
   overflow: hidden
---
+++ app/src/pages/home/components/Recommend.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <img class="item-img" src="//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg" alt="">
+        <div class="item-info">
+          <p class="item-title"></p>
+          <p class="item-desc"></p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend"
+}
+</script>
+
+<style lang="stylus" scoped>
+.title
+  margin-top .2rem
+  line-height: .8rem
+  background #eee
+  text-indent .2rem
+
+.item
+  overflow hidden
+  display flex
+  height 1.9rem
+
+  .item-img
+    width 1.7rem
+    weight 1.7rem
+    padding .1rem
+  .item-icon
+    flex 1
+</style>
