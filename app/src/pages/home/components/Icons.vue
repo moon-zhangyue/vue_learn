30521401b71482f9eaf110dc082e307961ecac99
zhangyue <575865770@qq.com>
2022-07-08 10:32:30 +0800

    refactor: icon图标排版分页和多余字隐藏

--- app/src/pages/home/components/Icons.vue
+++ app/src/pages/home/components/Icons.vue
@@ -1,64 +1,115 @@
 <template>
   <div class="icons">
-    <swiper :options="swiperOptions">
-      <div class="icon">
-        <div class="icon-img">
-          <img class='icon-img-content' src="https://picbed.qunarzz.com/01d2f57f920666364197a850dab859a8.png">
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class='icon-img-content' :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
         </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
-      <div class="icon">
-        <div class="icon-img">
-          <img class='icon-img-content' src="https://picbed.qunarzz.com/01d2f57f920666364197a850dab859a8.png">
-        </div>
-        <p class="icon-desc">热门景点</p>
-      </div>
+      </swiper-slide>
     </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HomeIcons"
+  name: "HomeIcons",
+  data() {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'https://picbed.qunarzz.com/01d2f57f920666364197a850dab859a8.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'https://picbed.qunarzz.com/c65b3bb7571a6bd62df669213e44b84d.png',
+        desc: '滑雪季节'
+      }, {
+        id: '0003',
+        imgUrl: 'https://picbed.qunarzz.com/25e3b9f17a21a6e0113c57a23ffccde4.png',
+        desc: '泡温泉'
+      }, {
+        id: '0004',
+        imgUrl: 'https://picbed.qunarzz.com/f6bb08a239ce1b038204120a8d1e4669.png',
+        desc: '动植物园'
+      }, {
+        id: '0005',
+        imgUrl: 'https://picbed.qunarzz.com/01d2f57f920666364197a850dab859a8.png',
+        desc: '景点门票'
+      }, {
+        id: '0006',
+        imgUrl: 'https://picbed.qunarzz.com/c65b3bb7571a6bd62df669213e44b84d.png',
+        desc: '滑雪季节'
+      }, {
+        id: '0007',
+        imgUrl: 'https://picbed.qunarzz.com/25e3b9f17a21a6e0113c57a23ffccde4.png',
+        desc: '泡温泉'
+      }, {
+        id: '0008',
+        imgUrl: 'https://picbed.qunarzz.com/f6bb08a239ce1b038204120a8d1e4669.png',
+        desc: '动植物园'
+      }, {
+        id: '0009',
+        imgUrl: 'https://img1.qunarzz.com/order/comp/2007/23/734c3c62d2adce02.png',
+        desc: '一日游'
+      }]
+    }
+  },
+  computed: {
+    pages() {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
-.icons
-  overflow: hidden
+@import '~styles/mixins.styl'
+.icons >>> .swiper-container
   height: 0
   padding-bottom 50%
 
-  .icon
-    position: relative
-    overflow: hidden
-    float left
-    width: 25%
-    height 0
-    padding-bottom 25%
+.icon
+  position: relative
+  overflow: hidden
+  float left
+  width: 25%
+  height 0
+  padding-bottom 25%
 
-    .icon-img
-      position: absolute
-      top 0
-      left 0
-      right 0
-      bottom .44rem
-      box-sizing border-box
-      padding .1rem
+  .icon-img
+    position: absolute
+    top 0
+    left 0
+    right 0
+    bottom .44rem
+    box-sizing border-box
+    padding .1rem
 
-      .icon-img-content
-        display: block
-        margin 0 auto
-        height: 100%
+    .icon-img-content
+      display: block
+      margin 0 auto
+      height: 100%
 
-    .icon-desc
-      position: absolute
-      left 0
-      right 0
-      bottom: 0
-      height .44rem
-      line-height .44rem
-      text-align: center
-      color $darkTextColor
+  .icon-desc
+    position: absolute
+    left 0
+    right 0
+    bottom: 0
+    height .44rem
+    line-height .44rem
+    text-align: center
+    color $darkTextColor
+    ellipsis()
 </style>
