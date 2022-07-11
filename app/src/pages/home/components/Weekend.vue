d275bc16362c9b028fbed7a815513106078a7557
zhangyue <575865770@qq.com>
2022-07-11 10:47:48 +0800

    refactor:热销推荐

--- app/src/pages/home/components/Weekend.vue
+++ app/src/pages/home/components/Weekend.vue
@@ -3,11 +3,11 @@
     <div class="title">热销推荐</div>
     <ul>
       <li class="item border-bottom" v-for="item of recommandList" :key="item.id"> <!--一像素边框-->
-        <img class="item-img" :src="item.imgUrl" alt="">
+        <div class="item-img-wrapper"><img class="item-img" :src="item.imgUrl" alt=""></div>
+
         <div class="item-info">
           <p class="item-title">{{ item.title }}</p>
           <p class="item-desc">{{ item.desc }}</p>
-          <button class="item-button">查看详情</button>
         </div>
       </li>
     </ul>
@@ -16,22 +16,22 @@
 
 <script>
 export default {
-  name: "HomeRecommend",
+  name: "HomeWeekend",
   data() {
     return {
       recommandList: [{
         id: '0001',
-        imgUrl: '//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
         title: '大连圣亚海洋世界',
         desc: '浪漫大连首战,浪漫的海洋主题乐园'
       }, {
         id: '0002',
-        imgUrl: '//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
         title: '大连圣亚海洋世界',
         desc: '浪漫大连首战,浪漫的海洋主题乐园 '
       }, {
         id: '0003',
-        imgUrl: '//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+        imgUrl: 'https://imgs.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
         title: '大连圣亚海洋世界',
         desc: '浪漫大连首战,浪漫的海洋主题乐园'
       }]
@@ -48,36 +48,24 @@ export default {
   background #eee
   text-indent .2rem
 
-.item
-  overflow hidden
-  display flex
-  height 1.9rem
+.item-img-wrapper
+  overflow: hidden
+  height 0
+  padding-bottom 33.9%
 
   .item-img
-    width 1.7rem
-    weight 1.7rem
-    padding .1rem
+    width 100%
 
-  .item-info
-    flex 1 //让所有弹性盒模型对象的子元素都有相同的长度，且忽略它们内部的内容
-    padding .1rem
-    min-width 0
+.item-info
+  padding .1rem
 
-    .item-title
-      line-height .54rem
-      font-size: .32rem
-      ellipsis()
+  .item-title
+    line-height .54rem
+    font-size: .32rem
+    ellipsis()
 
-    .item-desc
-      line-height .4rem
-      color #ccc
-      ellipsis()
-
-    .item-button
-      line-height .44rem
-      margin-top .16rem
-      background #ff9300
-      padding 0 .2rem
-      border-radius .06rem
-      color #fff
+  .item-desc
+    line-height .4rem
+    color #ccc
+    ellipsis()
 </style>
