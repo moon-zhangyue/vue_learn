57085ad9cc4f0168be1c7cad35e3e718b2d2a68c
zhangyue <575865770@qq.com>
2022-07-11 16:26:51 +0800

    refactor:城市列表

---
+++ app/src/pages/city/City.vue
@@ -0,0 +1,19 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+
+import CityHeader from './components/Header'
+
+export default {
+  name: "City",
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- app/src/pages/home/components/Header.vue
+++ app/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe60e;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{ this.city }}
-      <span class="iconfont arrow-icon">&#xe68f;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{ this.city }}
+        <span class="iconfont arrow-icon">&#xe68f;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
 @import '~styles/varibles.styl'
 .header
   display: flex
-  line-height: .86rem
+  line-height: $headerHeight
   background: $bgColor
   color: #fff
 
@@ -54,6 +56,7 @@ export default {
     width: 1.24rem
     floatL: right
     text-align: center
+    color: #fff
 
     .arrow-icon
       margin-left -.04rem
