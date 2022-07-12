282ac97e5f5c4de4d7ec995cbfe261412f09f607
zhangyue <575865770@qq.com>
2022-07-12 17:36:06 +0800

    refactor: 城市选择 字母列表

--- app/src/pages/city/City.vue
+++ app/src/pages/city/City.vue
@@ -2,13 +2,13 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
         <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -21,6 +21,29 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data() {
+        return {
+            cities: {},
+            hotCities: []
+        }
+    },
+    methods: {
+        getCityInfo() {
+            axios.get('./api/city.json')
+                .then(this.handleCityInfoSucc)
+        },
+        handleCityInfoSucc(res) {
+            res = res.data
+            if (res.ret && res.data) {
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        }
+    },
+    mounted() {
+        this.getCityInfo()
     }
 }
 </script>
