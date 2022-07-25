dd0d285abde18cafa0b1567d30b12fc52f18a714
zhangyue <575865770@qq.com>
2022-07-25 14:51:03 +0800

    refactor: transition 实现渐变效果

--- app/src/pages/detail/components/Banner.vue
+++ app/src/pages/detail/components/Banner.vue
@@ -9,16 +9,19 @@
                 </div>
                 <div class="banner-number">
                     <span class="iconfont banner-icon">&#xea83;</span>
-                    {{this.bannerImgs.length}}
+                    {{ this.bannerImgs.length }}
                 </div>
             </div>
         </div>
-        <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+        <fade-animation>
+            <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+        </fade-animation>
     </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 
 export default {
     name: "DetailBanner",
@@ -41,7 +44,8 @@ export default {
         }
     },
     components: {
-        CommonGallary
+        CommonGallary,
+        FadeAnimation
     }
 }
 </script>
--- app/src/pages/detail/components/Header.vue
+++ app/src/pages/detail/components/Header.vue
@@ -25,7 +25,7 @@ export default {
     },
     methods: {
         handleScroll() {
-            const top = document.documentElement.scrollTop
+            const top = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset
             if (top > 60) {
                 let opacity = top / 140
                 opacity = opacity > 1 ? 1 : opacity
@@ -36,10 +36,10 @@ export default {
             }
         }
     },
-    activated() {
+    mounted() {
         window.addEventListener("scroll", this.handleScroll)
     },
-    deactivated() {
+    destroyed() {
         window.removeEventListener("scroll", this.handleScroll) //解绑全局事件
     }
 }
